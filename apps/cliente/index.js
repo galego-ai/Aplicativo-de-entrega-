3e9e25aecd5b69d1b12663c79f1bbf89335cc9d4
galego-ai <elismarribeiro298@gmail.com>
2026-08-29 22:33:25 -0300

    feat: integrar chat ao app cliente

--- apps/cliente/index.js
+++ apps/cliente/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import { registerRootComponent } from "expo";
 import App from "./App";
 import NotificationHost from "./NotificationHost";
+import OrderChatHost from "./OrderChatHost";
 
 function Root(){
-  return <NotificationHost app="CUSTOMER" appIdentifier="br.com.clickfood.cliente"><App/></NotificationHost>;
+  return <NotificationHost app="CUSTOMER" appIdentifier="br.com.clickfood.cliente"><OrderChatHost><App/></OrderChatHost></NotificationHost>;
 }
 
 registerRootComponent(Root);
