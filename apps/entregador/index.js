1f6fb5f4968e61762f47c3f0a9c10da6be77e292
galego-ai <elismarribeiro298@gmail.com>
2026-08-30 15:49:44 -0300

    feat: exigir aceite legal no app entregador

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -7,9 +7,10 @@ import OrderChatHost from "./OrderChatHost";
 import DriverSupportHost from "./DriverSupportHost";
 import DriverPayoutHost from "./DriverPayoutHost";
 import AccountLifecycleHost from "./AccountLifecycle";
+import LegalConsentGate from "./LegalConsentGate";
 
 function Root(){
-  return <AccountLifecycleHost scheme="clickfood-entregador"><NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><DriverSupportHost><DriverPayoutHost><App/></DriverPayoutHost></DriverSupportHost></DriverDocumentsHost></OrderChatHost></NotificationHost></AccountLifecycleHost>;
+  return <AccountLifecycleHost scheme="clickfood-entregador"><NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><DriverSupportHost><DriverPayoutHost><App/><LegalConsentGate/></DriverPayoutHost></DriverSupportHost></DriverDocumentsHost></OrderChatHost></NotificationHost></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
