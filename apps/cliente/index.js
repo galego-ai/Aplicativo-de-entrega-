c519f74dcb00d995ca9ca33f4a5652ce7ec906c0
galego-ai <elismarribeiro298@gmail.com>
2026-08-30 15:49:30 -0300

    feat: exigir aceite legal no app cliente

--- apps/cliente/index.js
+++ apps/cliente/index.js
@@ -4,9 +4,10 @@ import App from "./App";
 import NotificationHost from "./NotificationHost";
 import OrderChatHost from "./OrderChatHost";
 import AccountLifecycleHost from "./AccountLifecycle";
+import LegalConsentGate from "./LegalConsentGate";
 
 function Root(){
-  return <AccountLifecycleHost scheme="clickfood-cliente"><NotificationHost app="CUSTOMER" appIdentifier="br.com.clickfood.cliente"><OrderChatHost><App/></OrderChatHost></NotificationHost></AccountLifecycleHost>;
+  return <AccountLifecycleHost scheme="clickfood-cliente"><NotificationHost app="CUSTOMER" appIdentifier="br.com.clickfood.cliente"><OrderChatHost><App/><LegalConsentGate/></OrderChatHost></NotificationHost></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
