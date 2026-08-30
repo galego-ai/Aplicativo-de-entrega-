d5c3289acfd7ee64b04cea8d106e3dfb6be06ece
galego-ai <elismarribeiro298@gmail.com>
2026-08-29 22:24:45 -0300

    feat: integrar documentos ao app entregador

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import { registerRootComponent } from "expo";
 import App from "./App";
 import NotificationHost from "./NotificationHost";
+import DriverDocumentsHost from "./DriverDocumentsHost";
 
 function Root(){
-  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><App/></NotificationHost>;
+  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><DriverDocumentsHost><App/></DriverDocumentsHost></NotificationHost>;
 }
 
 registerRootComponent(Root);
