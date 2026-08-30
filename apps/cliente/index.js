2e3d22b4274a0c4f49bda13a2705518e143a2248
galego-ai <elismarribeiro298@gmail.com>
2026-08-30 15:32:12 -0300

    feat: tratar recuperação de senha no cliente

--- apps/cliente/index.js
+++ apps/cliente/index.js
@@ -3,9 +3,10 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 import NotificationHost from "./NotificationHost";
 import OrderChatHost from "./OrderChatHost";
+import AccountLifecycleHost from "./AccountLifecycle";
 
 function Root(){
-  return <NotificationHost app="CUSTOMER" appIdentifier="br.com.clickfood.cliente"><OrderChatHost><App/></OrderChatHost></NotificationHost>;
+  return <AccountLifecycleHost scheme="clickfood-cliente"><NotificationHost app="CUSTOMER" appIdentifier="br.com.clickfood.cliente"><OrderChatHost><App/></OrderChatHost></NotificationHost></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
