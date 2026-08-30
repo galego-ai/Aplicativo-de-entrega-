080545daebe07898e107e1a3dcc69fe1753aec38
galego-ai <elismarribeiro298@gmail.com>
2026-08-29 22:33:31 -0300

    feat: integrar chat ao app entregador

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -3,9 +3,10 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 import NotificationHost from "./NotificationHost";
 import DriverDocumentsHost from "./DriverDocumentsHost";
+import OrderChatHost from "./OrderChatHost";
 
 function Root(){
-  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><DriverDocumentsHost><App/></DriverDocumentsHost></NotificationHost>;
+  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><App/></DriverDocumentsHost></OrderChatHost></NotificationHost>;
 }
 
 registerRootComponent(Root);
