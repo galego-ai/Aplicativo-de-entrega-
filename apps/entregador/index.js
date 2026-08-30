757e13039ede7d047ea904318ec8109e61a20489
galego-ai <elismarribeiro298@gmail.com>
2026-08-30 12:40:27 -0300

    feat: conectar carteira de repasses ao Entregador

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -5,9 +5,10 @@ import NotificationHost from "./NotificationHost";
 import DriverDocumentsHost from "./DriverDocumentsHost";
 import OrderChatHost from "./OrderChatHost";
 import DriverSupportHost from "./DriverSupportHost";
+import DriverPayoutHost from "./DriverPayoutHost";
 
 function Root(){
-  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><DriverSupportHost><App/></DriverSupportHost></DriverDocumentsHost></OrderChatHost></NotificationHost>;
+  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><DriverSupportHost><DriverPayoutHost><App/></DriverPayoutHost></DriverSupportHost></DriverDocumentsHost></OrderChatHost></NotificationHost>;
 }
 
 registerRootComponent(Root);
