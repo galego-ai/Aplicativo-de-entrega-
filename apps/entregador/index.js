2494771f67fd764e6a2768033170381942fd9f0c
galego-ai <elismarribeiro298@gmail.com>
2026-08-30 12:33:18 -0300

    feat: conectar suporte flutuante ao Entregador

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -4,9 +4,10 @@ import App from "./App";
 import NotificationHost from "./NotificationHost";
 import DriverDocumentsHost from "./DriverDocumentsHost";
 import OrderChatHost from "./OrderChatHost";
+import DriverSupportHost from "./DriverSupportHost";
 
 function Root(){
-  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><App/></DriverDocumentsHost></OrderChatHost></NotificationHost>;
+  return <NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><DriverSupportHost><App/></DriverSupportHost></DriverDocumentsHost></OrderChatHost></NotificationHost>;
 }
 
 registerRootComponent(Root);
