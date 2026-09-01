5cdec9ca26fe654d7d3c7fb5211b3e18e02cc522
galego-ai <elismarribeiro298@gmail.com>
2026-09-01 17:47:05 -0300

    feat(entregador): ativar bolinha flutuante no app

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -4,11 +4,13 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 import DriverProfessionalShell from "./DriverProfessionalShell";
 import DriverDocumentsHost from "./DriverDocumentsHost";
+import DriverFloatingBubbleHost from "./DriverFloatingBubbleHost";
+import DriverHomeResetHost from "./DriverHomeResetHost";
 import AccountLifecycleHost from "./AccountLifecycle";
 import LegalConsentGate from "./LegalConsentGate";
 
 function Root(){
-  return <AccountLifecycleHost scheme="clickfood-entregador"><DriverDocumentsHost><DriverProfessionalShell><App/></DriverProfessionalShell></DriverDocumentsHost><LegalConsentGate/></AccountLifecycleHost>;
+  return <AccountLifecycleHost scheme="clickfood-entregador"><DriverFloatingBubbleHost><DriverHomeResetHost><DriverDocumentsHost><DriverProfessionalShell><App/></DriverProfessionalShell></DriverDocumentsHost></DriverHomeResetHost></DriverFloatingBubbleHost><LegalConsentGate/></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
