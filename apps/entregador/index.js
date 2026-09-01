d341f87c7744aa9284f6d39b052382048444167f
galego-ai <elismarribeiro298@gmail.com>
2026-09-01 17:27:42 -0300

    feat(entregador): mover chat notificações suporte para menu profissional

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -2,15 +2,13 @@ import "./BackgroundLocation";
 import React from "react";
 import { registerRootComponent } from "expo";
 import App from "./App";
-import NotificationHost from "./NotificationHost";
+import DriverProfessionalShell from "./DriverProfessionalShell";
 import DriverDocumentsHost from "./DriverDocumentsHost";
-import OrderChatHost from "./OrderChatHost";
-import DriverSupportHost from "./DriverSupportHost";
 import AccountLifecycleHost from "./AccountLifecycle";
 import LegalConsentGate from "./LegalConsentGate";
 
 function Root(){
-  return <AccountLifecycleHost scheme="clickfood-entregador"><NotificationHost app="DRIVER" appIdentifier="br.com.clickfood.entregador"><OrderChatHost><DriverDocumentsHost><DriverSupportHost><App/><LegalConsentGate/></DriverSupportHost></DriverDocumentsHost></OrderChatHost></NotificationHost></AccountLifecycleHost>;
+  return <AccountLifecycleHost scheme="clickfood-entregador"><DriverDocumentsHost><DriverProfessionalShell><App/></DriverProfessionalShell></DriverDocumentsHost><LegalConsentGate/></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
