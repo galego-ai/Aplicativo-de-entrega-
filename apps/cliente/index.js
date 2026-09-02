cc0a97519f4e9531221a360209f83b2715803cf7
galego-ai <elismarribeiro298@gmail.com>
2026-09-01 22:35:31 -0300

    feat(cliente): montar carteira de cartões no app

--- apps/cliente/index.js
+++ apps/cliente/index.js
@@ -4,9 +4,10 @@ import App from "./App";
 import CustomerProfessionalShell from "./CustomerProfessionalShell";
 import AccountLifecycleHost from "./AccountLifecycle";
 import LegalConsentGate from "./LegalConsentGate";
+import SavedCardsHost from "./SavedCardsHost";
 
 function Root(){
-  return <AccountLifecycleHost scheme="clickfood-cliente"><CustomerProfessionalShell><App/></CustomerProfessionalShell><LegalConsentGate/></AccountLifecycleHost>;
+  return <AccountLifecycleHost scheme="clickfood-cliente"><SavedCardsHost><CustomerProfessionalShell><App/></CustomerProfessionalShell></SavedCardsHost><LegalConsentGate/></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
