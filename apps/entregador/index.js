492d7ffaa6f6b043f0c92d44360622de2050ea20
galego-ai <elismarribeiro298@gmail.com>
2026-09-03 15:15:29 +0000

    feat(mobile): aplicar layout aprovado Cliente e Entregador

--- apps/entregador/index.js
+++ apps/entregador/index.js
@@ -6,12 +6,11 @@ import DriverProfessionalShell from "./DriverProfessionalShell";
 import DriverDocumentsHost from "./DriverDocumentsHost";
 import DriverFloatingBubbleHost from "./DriverFloatingBubbleHost";
 import DriverHomeResetHost from "./DriverHomeResetHost";
-import DriverIdleHomeShell from "./DriverIdleHomeShell";
 import AccountLifecycleHost from "./AccountLifecycle";
 import LegalConsentGate from "./LegalConsentGate";
 
 function Root(){
-  return <AccountLifecycleHost scheme="clickfood-entregador"><DriverFloatingBubbleHost><DriverHomeResetHost><DriverDocumentsHost><DriverProfessionalShell><DriverIdleHomeShell><App/></DriverIdleHomeShell></DriverProfessionalShell></DriverDocumentsHost></DriverHomeResetHost></DriverFloatingBubbleHost><LegalConsentGate/></AccountLifecycleHost>;
+  return <AccountLifecycleHost scheme="clickfood-entregador"><DriverFloatingBubbleHost><DriverHomeResetHost><DriverDocumentsHost><DriverProfessionalShell><App/></DriverProfessionalShell></DriverDocumentsHost></DriverHomeResetHost></DriverFloatingBubbleHost><LegalConsentGate/></AccountLifecycleHost>;
 }
 
 registerRootComponent(Root);
